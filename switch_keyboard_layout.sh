d3b6dfab21d6e8b2ce258f895ef91923993d2af9
Suracheth Chawla <me@suracheth.com>
2024-09-19 17:15:48 +0700

    save raw file instead rather than link

--- switch_keyboard_layout.sh
+++ switch_keyboard_layout.sh
@@ -1,1 +1,18 @@
-/home/suracheth/.config/leftwm/switch_keyboard_layout.sh
+#!/bin/bash
+
+current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
+
+if [ "$current_layout" == "us" ]; then
+    setxkbmap th
+    notify-send "Keyboard Layout" "Switched to Thai"
+else
+    setxkbmap us
+    notify-send "Keyboard Layout" "Switched to English"
+fi
+
+# Set English as default if no layout is set
+if [ -z "$current_layout" ]; then
+    setxkbmap us
+    notify-send "Keyboard Layout" "Set to default (English)"
+fi
+
